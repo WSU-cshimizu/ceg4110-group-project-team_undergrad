2581833e444ededa46f794a7771ea3c88f6b13f4
vHoui <143646375+vHoui@users.noreply.github.com>
2024-10-25 09:37:32 -0400

    Update FirstEx.java
    Cleaned up some code. Adjusted some parts to incorporate future code.

--- deliverables/Project_work/FirstEx.java
+++ deliverables/Project_work/FirstEx.java
@@ -27,7 +27,8 @@ public class FirstEx extends Application {
         initUI(stage);
     }
 
-    private void initUI(Stage stage) {
+    // The actual start of the intialization
+    public static void initUI(Stage stage) {
 
         // Base pane
         StackPane root = new StackPane();
@@ -41,31 +42,38 @@ public class FirstEx extends Application {
         Button btnBotDifficulty = new Button("Bot Difficulty");
         Button btnSettings = new Button("Settings");
         Button btnSeeStats = new Button("See Stats");
+        Button btnExit = new Button("Exit");
 
         // Set button size
         setButtonSize(btnStartGame);
         setButtonSize(btnBotDifficulty);
         setButtonSize(btnSettings);
         setButtonSize(btnSeeStats);
+        setButtonSize(btnExit);
 
         // Exit action for buttons (temporary, for testing)
         // btnStartGame.setOnAction((ActionEvent event) -> Platform.exit());
         btnStartGame.setOnAction((ActionEvent event) -> {
+            // TODO: Make further implementation
             showGameScene(stage); // Call method to switch scenes
         });
         btnBotDifficulty.setOnAction((ActionEvent event) -> {
-            showBotDifficulty(stage); // Call method to switch scenes
+            botDifficulty.showBotDifficulty(stage); // Call method to switch scenes
         });
         btnSettings.setOnAction((ActionEvent event) -> {
+            // TODO: Make further implementation
             showSettingsScences(stage); // Call method to switch scenes
         });
         btnSeeStats.setOnAction((ActionEvent event) -> {
-            // showStats(stage); // Call method to switch scenes
+            // TODO: Make further implementation
             stats.showStats(stage);
         });
+        btnExit.setOnAction((ActionEvent event) -> Platform.exit());
+
+        
 
         // Adding buttons to VBox
-        vBox.getChildren().addAll(btnStartGame, btnBotDifficulty, btnSettings, btnSeeStats);
+        vBox.getChildren().addAll(btnStartGame, btnBotDifficulty, btnSettings, btnSeeStats, btnExit);
 
         // Adding VBox to the root pane
         root.getChildren().add(vBox);
@@ -73,7 +81,7 @@ public class FirstEx extends Application {
         // Set background color or image (choose one)
         setBackgroundColor(root, Color.LIGHTBLUE); // You can change the color here
         // setBackgroundImage(root, "path_to_image.jpg"); // Set your image path here
-        // setBackgroundImage(root, "test.png");
+        setBackgroundImage(root, "test.png");
 
         // Scene setup
         Scene scene = new Scene(root, 1000, 800);
@@ -85,25 +93,25 @@ public class FirstEx extends Application {
     }
 
     // Method to set the button size
-    private void setButtonSize(Button button) {
+    private static void setButtonSize(Button button) {
         button.setPrefWidth(200); // Width of the button
         button.setPrefHeight(50); // Height of the button
     }
 
     // Method to set background color
-    private void setBackgroundColor(StackPane pane, Paint color) {
+    private static void setBackgroundColor(StackPane pane, Paint color) {
         pane.setBackground(new Background(new BackgroundFill(color, CornerRadii.EMPTY, null)));
     }
 
     // Method to set background image
-    private void setBackgroundImage(StackPane pane, String imagePath) {
+    private static void setBackgroundImage(StackPane pane, String imagePath) {
         Image image = new Image(imagePath);
         BackgroundImage bgImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
         pane.setBackground(new Background(bgImage));
     }
 
     // Method to switch scenes
-    private void showGameScene(Stage stage) {
+    private static void showGameScene(Stage stage) {
         // Create new pane for the game
         StackPane gamePane = new StackPane();
         Scene gameScene = new Scene(gamePane, 1000, 800);
@@ -117,21 +125,7 @@ public class FirstEx extends Application {
         stage.show();
     }
 
-    private void showSettingsScences(Stage stage) {
-
-        StackPane settingsPane = new StackPane();
-        Scene settingScene = new Scene(settingsPane, 1000, 800);
-
-        // Add some example content (this is where your game logic would go)
-        Label settingsLabel = new Label("Game Started!");
-        settingsPane.getChildren().add(settingsLabel);
-
-        // Set the new scene to the stage
-        stage.setScene(settingScene);
-        stage.show();
-    }
-
-    private void showBotDifficulty(Stage stage) {
+    private static void showSettingsScences(Stage stage) {
 
         StackPane settingsPane = new StackPane();
         Scene settingScene = new Scene(settingsPane, 1000, 800);
