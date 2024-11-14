c7b628bbdc336c8a57b08f3cfc7f1e0b85b3328a
vHoui <143646375+vHoui@users.noreply.github.com>
2024-11-14 11:19:01 -0500

    Update FirstEx.java
    Updated FirstEx.java

--- deliverables/Project_work/FirstEx.java
+++ deliverables/Project_work/FirstEx.java
@@ -12,6 +12,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
@@ -37,12 +38,19 @@ public class FirstEx extends Application {
         VBox vBox = new VBox(20); // 20 is the spacing between the buttons
         vBox.setAlignment(Pos.CENTER); // Align VBox in the center
 
+        VBox right = new VBox(20);
+        right.setAlignment(Pos.BOTTOM_RIGHT);
+
+        HBox logoutBox = new HBox();
+        logoutBox.setAlignment(Pos.BOTTOM_RIGHT);
+
         // Creating buttons with specific names
         Button btnStartGame = new Button("Start Game");
         Button btnBotDifficulty = new Button("Bot Difficulty");
         Button btnSettings = new Button("Settings");
         Button btnSeeStats = new Button("See Stats");
         Button btnExit = new Button("Exit");
+        Button btnLogout = new Button("Logout");
 
         // Set button size
         setButtonSize(btnStartGame);
@@ -50,12 +58,15 @@ public class FirstEx extends Application {
         setButtonSize(btnSettings);
         setButtonSize(btnSeeStats);
         setButtonSize(btnExit);
+        setButtonSize(btnLogout);
 
         // Exit action for buttons (temporary, for testing)
         // btnStartGame.setOnAction((ActionEvent event) -> Platform.exit());
         btnStartGame.setOnAction((ActionEvent event) -> {
             // TODO: Make further implementation
-            showGameScene(stage); // Call method to switch scenes
+            // CheckersBoardGUI.StartUI(stage);
+            CheckersBoardGUI test = new CheckersBoardGUI(); //
+            test.start(stage);
         });
         btnBotDifficulty.setOnAction((ActionEvent event) -> {
             botDifficulty.showBotDifficulty(stage); // Call method to switch scenes
@@ -70,18 +81,56 @@ public class FirstEx extends Application {
         });
         btnExit.setOnAction((ActionEvent event) -> Platform.exit());
 
-        
+ 
+        btnLogout.setOnAction((ActionEvent event) -> {
+            loginPage test = new loginPage();
+            test.start(stage);
+        });
 
         // Adding buttons to VBox
         vBox.getChildren().addAll(btnStartGame, btnBotDifficulty, btnSettings, btnSeeStats, btnExit);
 
+        // right.getChildren().addAll(btnLogout);
+        logoutBox.getChildren().add(btnLogout);
+
+
         // Adding VBox to the root pane
-        root.getChildren().add(vBox);
+       // Adding VBox to the root pane
+        root.getChildren().add(vBox); // Centered by default in StackPane
+        // root.getChildren().add(logoutBox); // Align to bottom-right
+
+        // Set the alignment for logoutBox
+        StackPane.setAlignment(logoutBox, Pos.BOTTOM_RIGHT);
+
+        
 
         // Set background color or image (choose one)
         setBackgroundColor(root, Color.LIGHTBLUE); // You can change the color here
         // setBackgroundImage(root, "path_to_image.jpg"); // Set your image path here
         setBackgroundImage(root, "test.png");
+
+
+        // // Create the logout button
+        // Button btnLogout = new Button("Logout");
+        // setButtonSize(btnLogout);
+
+        // // Handle the logout action
+        // btnLogout.setOnAction(e -> {
+        //     loginPage loginPage = new loginPage();
+        //     try {
+        //         loginPage.start(stage); // Return to the login page
+        //     } catch (Exception ex) {
+        //         ex.printStackTrace();
+        //     }
+        // });
+
+        // // Place the logout button in the bottom-right corner
+        // HBox logoutBox = new HBox();
+        // logoutBox.setAlignment(Pos.BOTTOM_RIGHT);
+        // logoutBox.getChildren().add(btnLogout);
+        // StackPane.setAlignment(logoutBox, Pos.BOTTOM_RIGHT); // Position it at the bottom-right of the StackPane
+        // root.getChildren().add(logoutBox);
+        
 
         // Scene setup
         Scene scene = new Scene(root, 1000, 800);
